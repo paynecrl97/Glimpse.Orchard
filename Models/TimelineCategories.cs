1e33318358557c77b619f19158b2b35dc60ef691
paynecrl97 <chris.payne@bedegaming.com>
2014-10-06 09:29:04 +0100

    updated timeline colours

--- Models/TimelineCategories.cs
+++ Models/TimelineCategories.cs
@@ -7,9 +7,9 @@ namespace Glimpse.Orchard.Models
         public static TimelineCategoryItem Widgets = new TimelineCategoryItem("Widgets", "#3939AA", "#595980");
         public static TimelineCategoryItem Layers = new TimelineCategoryItem("Layers", "#AA3939", "#805959");
         public static TimelineCategoryItem Shapes = new TimelineCategoryItem("Shapes", "#39AA39", "#598059");
-        public static TimelineCategoryItem Parts = new TimelineCategoryItem("Parts", "#AAAA39", "#808059");
-        public static TimelineCategoryItem Fields = new TimelineCategoryItem("Fields", "#39AAAA", "#598080");
+        public static TimelineCategoryItem Fields = new TimelineCategoryItem("Fields", "#AAAA39", "#808059");
+        public static TimelineCategoryItem Parts = new TimelineCategoryItem("Parts", "#FFA256", "#A65A1C");
         public static TimelineCategoryItem ContentManagement = new TimelineCategoryItem("Content Manager", "#AA39AA", "#805980");
-        public static TimelineCategoryItem Authorization = new TimelineCategoryItem("Authorization", "#393939", "#595959");
+        public static TimelineCategoryItem Authorization = new TimelineCategoryItem("Authorization", "#C50080", "#800053");
     }
 }
