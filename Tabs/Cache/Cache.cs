e53b380e537b7a0d0f56b503cad8d78d30d7464a
paynecrl97 <chris.payne@bedegaming.com>
2014-12-18 21:51:50 +0000

    added cache tab

---
+++ Tabs/Cache/Cache.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Extensions;
+using Glimpse.Core.Message;
+using Glimpse.Core.Tab.Assist;
+
+namespace Glimpse.Orchard.Tabs.Cache
+{
+    public class CacheMessage : MessageBase
+    {
+        public string Action { get; set; }
+        public string Key { get; set; }
+        public object Value { get; set; }
+        public string Result { get; set; }
+        public TimeSpan ValidFor { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+
+    public class CacheTab : TabBase, ITabSetup, IKey
+    {
+
+        public override object GetData(ITabContext context)
+        {
+            return context.GetMessages<CacheMessage>();
+        }
+
+        public override string Name
+        {
+            get { return "Cache"; }
+        }
+
+        public void Setup(ITabSetupContext context)
+        {
+            context.PersistMessages<CacheMessage>();
+        }
+
+        public string Key
+        {
+            get { return "glimpse_orchard_cache"; }
+        }
+    }
+
+    public class CacheMessagesConverter : SerializationConverter<IEnumerable<CacheMessage>>
+    {
+        public override object Convert(IEnumerable<CacheMessage> messages)
+        {
+            var root = new TabSection("Action", "Key", "Result", "Value", "Time Taken");
+            foreach (var message in messages.OrderByDescending(m=>m.Duration.TotalMilliseconds))
+            {
+                root.AddRow()
+                    .Column(message.Action)
+                    .Column(message.Key)
+                    .Column(message.Result)
+                    .Column(message.Value)
+                    .Column(string.Format("{0} ms", Math.Round(message.Duration.TotalMilliseconds, 2)));
+            }
+
+            return root.Build();
+        }
+    }
+}
