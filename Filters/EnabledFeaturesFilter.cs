d8c4011d654b87a7f0008c85c6804edd83f912d7
paynecrl97 <chris.payne@bedegaming.com>
2015-01-09 10:40:22 +0000

    added enabled featires and site settings tabs

---
+++ Filters/EnabledFeaturesFilter.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Glimpse.Orchard.PerfMon.Services;
+using Glimpse.Orchard.Tabs.EnabledFeatures;
+using Orchard.Environment.Extensions;
+using Orchard.Environment.Features;
+using Orchard.Mvc.Filters;
+
+namespace Glimpse.Orchard.Filters
+{
+    [OrchardFeature("Glimpse.Orchard.EnabledFeatures")]
+    public class EnabledFeaturesFilter : FilterProvider, IResultFilter
+    {
+        private readonly IFeatureManager _featureManager;
+        private readonly IPerformanceMonitor _performanceMonitor;
+
+        public EnabledFeaturesFilter(IFeatureManager featureManager, IPerformanceMonitor performanceMonitor) {
+            _featureManager = featureManager;
+            _performanceMonitor = performanceMonitor;
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext) {}
+
+        public void OnResultExecuted(ResultExecutedContext filterContext) {
+            var enabledFeatures = _featureManager.GetEnabledFeatures();
+
+            foreach (var feature in enabledFeatures) {
+                _performanceMonitor.PublishMessage(new EnabledFeatureMessage {
+                    Category = feature.Category,
+                    Description = feature.Description,
+                    Extension = feature.Extension,
+                    FeatureId = feature.Id,
+                    Name = feature.Name,
+                    Priority = feature.Priority
+                });
+            }
+        }
+    }
+}
---
+++ Tabs/SiteSettings/SiteSettings.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Extensions;
+using Glimpse.Core.Message;
+using Glimpse.Core.Tab.Assist;
+
+namespace Glimpse.Orchard.Tabs.SiteSettings
+{
+    public class SiteSettingsMessage : MessageBase
+    {
+        public string Part { get; set; }
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+
+    public class EnabledFeaturesTab : TabBase, ITabSetup, IKey
+    {
+        public override object GetData(ITabContext context)
+        {
+            if (context.GetMessages<SiteSettingsMessage>().Any())
+            {
+                return context.GetMessages<SiteSettingsMessage>().ToList();
+            }
+
+            return "There is no data available for this tab, check that the 'Glimpse for Orchard Site Settings' feature is enabled.";
+        }
+
+        public override string Name
+        {
+            get { return "Site Settings"; }
+        }
+
+        public void Setup(ITabSetupContext context)
+        {
+            context.PersistMessages<SiteSettingsMessage>();
+        }
+
+        public string Key
+        {
+            get { return "glimpse_orchard_sitesettings"; }
+        }
+    }
+
+    public class EnabledFeatureMessagesConverter : SerializationConverter<IEnumerable<SiteSettingsMessage>>
+    {
+        public override object Convert(IEnumerable<SiteSettingsMessage> messages)
+        {
+            var root = new TabSection("Part", "Name", "Value");
+            foreach (var message in messages.OrderBy(m => m.Part).ThenBy(m => m.Name))
+            {
+                root.AddRow()
+                    .Column(message.Part)
+                    .Column(message.Name)
+                    .Column(message.Value);
+            }
+
+            return root.Build();
+        }
+    }
+}
